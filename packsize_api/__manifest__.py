5a5d6687a2d1047abbb6f7707e70b272eaad864e
Elvic571 <elkin@doctordiesel.org>
2025-05-11 19:58:34 -0400

    Packsize_api upload

---
+++ packsize_api/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Packsize API Connector',
+    'version': '17.0.1.0.0',
+    'category': 'Manufacturing',
+    'summary': 'Connect Odoo with Packsize on-demand box machines',
+    'author': 'doctordiedel',
+    'website': 'https://www.doctordiesel.org',
+    'depends': ['base', 'mrp'],
+    'data': [
+
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+    'auto_install': False,
+    'description': """
+This module integrates Odoo with Packsize Packnet Import API.
+    """,
+}
